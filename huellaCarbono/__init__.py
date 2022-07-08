433bf6bec31c0120ddd0b67ce17ff70f8e92ff6d
Noodle96 <jorgealfredomarino2@gmail.com>
2022-07-08 03:24:41 -0500

    Registrar Usuario , auth, Blueprint

--- huellaCarbono/__init__.py
+++ huellaCarbono/__init__.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 # cargando las configuraciones
 app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
+
+
+#IMPORTAR VISTAS
+from huellaCarbono.views.auth import auth
+app.register_blueprint(auth)
+
 db.create_all()
