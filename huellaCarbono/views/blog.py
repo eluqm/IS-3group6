fd7b6053da134aa81237b205accb815de67f0987
Noodle96 <jorgealfredomarino2@gmail.com>
2022-07-12 02:28:10 -0500

    agregando CRUD completo del post

--- huellaCarbono/views/blog.py
+++ huellaCarbono/views/blog.py
@@ -29,7 +29,7 @@ def index():
 # REGISTRAR UN POST
 @blog.route('/blog/create', methods=('GET', 'POST'))
 @login_required
-def crearPost():
+def createPost():
     if request.method == 'POST':
         title = request.form.get('title')
         body = request.form.get('body')
@@ -43,3 +43,41 @@ def crearPost():
             db.session.commit()
             return redirect(url_for('blog.index'))
     return render_template('blog/create.html')
+
+
+# OBTENER POST
+def get_post(id, check_author=True):
+    post = Post.query.get(id)
+    if post is None:
+        abort(404, f'Id{id} de la publicaion no existe')
+    if check_author and post.author != g.user.id:
+        abort(404)
+    return post
+
+
+# UPDATE POST
+@blog.route('/blog/update/<int:id>', methods=('GET', 'POST'))
+@login_required
+def updatePost(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        post.title = request.form.get('title')
+        post.body = request.form.get('body')
+
+        if not post.title:
+            flash('Se requiere un título')
+        else:
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('blog.index'))
+    return render_template('blog/update.html', post=post)
+
+
+# ELIMINAR POST
+@blog.route('/blog/delete/<int:id>', methods=('GET', 'POST'))
+@login_required
+def deletePost(id):
+    post = get_post(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('blog.index'))
