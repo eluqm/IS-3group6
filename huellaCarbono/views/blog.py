5f28a8c9998a486ad5f3233727feb8d95d3b54af
Noodle96 <jorgealfredomarino2@gmail.com>
2022-07-11 23:58:14 -0500

    creando funcion crearPost

--- huellaCarbono/views/blog.py
+++ huellaCarbono/views/blog.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import(
     render_Template, Blueprint, flash, g, redirect, request, url_for
 )
@@ -23,3 +24,22 @@ def index():
     posts = Post.query.all()
     db.session.commit()
     return render_Template('blog/index.html', posts=posts)
+
+
+# REGISTRAR UN POST
+@blog.route('/blog/create', methods=('GET', 'POST'))
+@login_required
+def crearPost():
+    if request == 'POST':
+        title = request.form.get('title')
+        body = request.form.get('body')
+        post = Post(g.user.id, title, body)
+
+        error = None
+        if not title:
+            flash('Se requiere un título')
+        else:
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('blog.index'))
+    return render_Template('blog/create.html')
