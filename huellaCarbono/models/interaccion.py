b620d18fbfbdbfa0659ee3c14fccbfc176fadc12
Noodle96 <jorgealfredomarino2@gmail.com>
2022-07-13 14:09:42 -0500

    Agregando tabla-model interaccion

---
+++ huellaCarbono/models/interaccion.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from huellaCarbono import db
+
+
+class Interaccion(db.Model):
+    __tablename__ = "interacciones"
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post = db.Column(db.Integer, db.ForeignKey('posts.id'), nuallable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, user, post) -> None:
+        self.user = user
+        self.post = user
