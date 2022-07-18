93b4b4e4ce8effbb69e881fda58240775367d000
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-07-18 18:42:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,4 @@ from huellaCarbono import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-#from flask import Flask
-#app = Flask(__name__)
-
-
-# qapp.route('/')
-# ef hola():
-#    return "<h1>Welcome to Huella carbono...</h1>"
+    
